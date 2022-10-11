eed5f65e5cbf2155d4e0b836c230dd9f803ac44c
aaronmitchell96 <107212540+aaronmitchell96@users.noreply.github.com>
2022-10-10 23:45:08 -0400

    jeopardy

--- section-1/jeopardy/jeopardy.js
+++ section-1/jeopardy/jeopardy.js
@@ -18,7 +18,6 @@
 //    ...
 //  ]
 
-let categories = [];
 
 
 /** Get NUM_CATEGORIES random category from API.
@@ -43,56 +42,3 @@ function getCategoryIds() {
 
 function getCategory(catId) {
 }
-
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
-
-async function fillTable() {
-}
-
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
-
-function handleClick(evt) {
-}
-
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
-
-function showLoadingView() {
-
-}
-
-/** Remove the loading spinner and update the button used to fetch data. */
-
-function hideLoadingView() {
-}
-
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
-
-async function setupAndStart() {
-}
-
-/** On click of start / restart button, set up game. */
-
-// TODO
-
-/** On page load, add event handler for clicking clues */
-
-// TODO
