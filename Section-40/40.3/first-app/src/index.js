2fb34e23d2d657d1bf33339a2f9fef0ee7cc9211
aaronmitchell96 <107212540+aaronmitchell96@users.noreply.github.com>
2023-05-15 15:23:12 -0400

    pokedex

--- Section-40/40.3/first-app/src/index.js
+++ Section-40/40.3/first-app/src/index.js
@@ -1,12 +1,17 @@
-import fruits from './fruits'
-import {choice, remove} from './helpers'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-let fruit = choice(fruits)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-console.log(`id like one ${fruit}, please`)
-console.log(`Here you go: ${fruit}`)
-console.log(`Delicious! May I have another?`)
-
-let remaining = remove(fruit, fruits)
-
-console.log(`Im sorry, we're all out. We have ${remaining.length} left.`)
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
